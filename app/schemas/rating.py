fdbc9acc9a111e6a650c981bb36abf12a346af6b
Kwabena Twumasi <ktwum721@gmail.com>
2024-08-26 18:34:54 +0000

    Rating schema added

---
+++ app/schemas/rating.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Rating module
+"""
+from pydantic import BaseModel
+
+
+class RatingBase(BaseModel):
+    """
+    Rating validation
+    """
+    value: int
+
+
+class RatingCreate(RatingBase):
+    """
+    Validate rating creation
+    """
+    pass
+
+
+class RatingOut(RatingBase):
+    """
+    Validate ratings out
+    """
+    id: int
+    user_id: int
+    resource_id: int
+
+    class Config:
+        orm_mode = True
