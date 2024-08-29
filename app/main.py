26008529da9a8ccbb99fee2c9e75be9957fef9d0
Kwabena Twumasi <ktwum721@gmail.com>
2024-08-29 01:12:45 +0000

    Authentication modified

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@
 This is the entry point of the application
 """
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
 from app.routes import (
         user, resource,
         review, rating
@@ -15,6 +16,7 @@ from app.logger import setup_logger
 
 app = FastAPI()
 logger = setup_logger("main")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
 engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(bind=engine)
